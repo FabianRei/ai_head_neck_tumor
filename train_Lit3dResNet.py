21a88753706cb849d9bc3f8cc8edbf798cca8ed5
FabianRei <F.abi.an@gmx.de>
2023-02-28 13:26:08 +0100

    update

--- train_Lit3dResNet.py
+++ train_Lit3dResNet.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import pytorch_lightning as pl
 import pickle
 import argparse
+import socket
 
 from data_loading.data_set import EcatDFDataset, get_default_tio_transform
 from torch.utils.data import DataLoader
@@ -20,7 +21,7 @@ from deep_learning.Lit3dResnet import Lit3dResnet
 parser = argparse.ArgumentParser()
 parser.add_argument('-col_id', '--col_id', help='Column index to excecute', default=0)
 parser.add_argument('-cv_split', '--cv_split', help='Cross validation split', default=0)
-parser.add_argument('-run_id', '--run_id', help='Run id', default=2)
+parser.add_argument('-run_id', '--run_id', help='Run id', default=1)
 args = vars(parser.parse_args())
 
 col_id = int(args['col_id'])
@@ -32,23 +33,32 @@ indices = [[0], [2], [0, 1], [2, 3], [0, 1, 2, 3], [0, 2], [0, 2, 3]]
 img_columns = [columns[i] for i in indices[col_id]]
 info_cols = ''.join(img_columns)
 
-
+###############################################
+# Run 1
 bs = 8
-info = f'Lit3dResnet_50_1chan_27feb23_{info_cols}_cv_{cv_split}_run_{run_id}'
 lr = 0.001
 adam_regularization = 0.01
 lr_decay = 0.95
 resnet_model_size = 50
 model_chan_in = len(img_columns)
 n_classes = 2
-epochs = 10
+epochs = 50
+
+info = f'Lit3dResnet_50_1chan_28feb23_{info_cols}_cv_{cv_split}_{epochs}_epochs_run_{run_id}'
 
 
 
 
-path_df = Path('/home/fabian/projects/phd/ai_radiation_therapy/ai_head_neck_tumor/data/clinical_data_has_CT_PT_21feb23.feather')
-df = pd.read_feather(path_df)
-data_path = Path(df.iloc[0].blue_data_path)
+if socket.gethostname() == 'blue':
+    path_df = Path('/home/fabian/projects/phd/ai_radiation_therapy/ai_head_neck_tumor/data/clinical_data_has_CT_PT_21feb23.feather')
+    df = pd.read_feather(path_df)
+    data_path = Path(df.iloc[0].blue_data_path)
+else:
+    path_df = Path("/home/freith/projects/ai_head_neck_tumor/data/clinical_data_has_CT_PT_21feb23.feather")
+    df = pd.read_feather(path_df)
+    data_path = Path(df.iloc[0].mdc_data_path)
+path_checkpoints = path_df.parent / 'checkpoints'
+
 
 
 # Data stuff
@@ -59,13 +69,12 @@ dl_valid = DataLoader(ds_valid, batch_size=bs, shuffle=False, num_workers=8)
 
 # Model & training stuff
 params = {'lr': lr, 'adam_regularization': adam_regularization, 'lr_decay': lr_decay, 'resnet_model_size': resnet_model_size, 
-          'model_chan_in': 1, 'n_classes': 2, 'bs': bs, 'img_column': img_column, 'data_path': data_path, 'path_df': path_df}
+          'model_chan_in': 1, 'n_classes': 2, 'bs': bs, 'img_column': img_columns, 'data_path': data_path, 'path_df': path_df}
 model = Lit3dResnet(params)
-trainer = pl.Trainer(gpus=1, max_epochs=epochs, enable_checkpointing=False)
+trainer = pl.Trainer(gpus=1, max_epochs=epochs, enable_checkpointing=False, default_root_dir=path_checkpoints)
 trainer.fit(model, dl_train, dl_valid)
 
 # Saving stuff
-path_checkpoints = Path('/home/fabian/projects/phd/ai_radiation_therapy/ai_head_neck_tumor/data/checkpoints')
 params['loss_acc_dict'] = model.loss_acc_dict
 trainer.save_checkpoint(path_checkpoints / f"{info}.ckpt")
 pickle.dump(params, open(path_checkpoints / f"{info}_params.pkl", "wb"))
